91fb88549898aef46b60167b0a340fa37ab80065
Sir Code <taylorschneider07@yahoo.com>
2023-02-18 09:31:30 -0500

    Add some models and textures

--- src/main/java/net/scmods/Main.java
+++ src/main/java/net/scmods/Main.java
@@ -1,14 +1,21 @@
 package net.scmods;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+import java.util.Dictionary;
 
 public class Main implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MODID = "consistentimprovements";
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+
+//		public static final ItemGroup CIM_GROUP = FabricItemGroupBuilder.create(new Identifier(MODID, "conimp_itemgroup"))
+//				.icon(() -> new ItemStack(typeLists.get(0).get(6)))
+//				.build();
 	}
 }
